98c7dafd1fee11416a16a4d298ad3df99355b56b
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-10-24 05:38:35 -0500

    Initial Commit

---
+++ gesture/gestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestureApp.swift
+//  gesture
+//
+//  Created by cisstudent on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct gestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
